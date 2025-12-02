6c4d9214fa3619a5dbdbaf8de24fab5ee0e16d90
CoccusQ <150150207+CoccusQ@users.noreply.github.com>
2025-12-03 00:57:35 +0800

    增加了绘图功能

--- puncta.c
+++ puncta.c
@@ -1,5 +1,6 @@
 #include <math.h>
 #include "coc.h"
+#include "coc_canvas.h"
 
 typedef struct Number {
     long long int_value;
@@ -413,6 +414,7 @@ void act_dec(Number *n) {
 }
 
 void act_input(Number *n) {
+    putchar('>');
     char buf[128];
     if (!fgets(buf, sizeof(buf), stdin)) {
         coc_log(COC_ERROR, "Input error");
@@ -458,6 +460,78 @@ static inline void register_builtin_actions(Interpreter *intp) {
     register_act(intp, "input", act_input);
 }
 
+Coc_Canvas canvas = {0};
+Coc_Color draw_color = BLACK;
+
+void act_gfx_plot(Number *n) {
+    long long pos = n->is_float ? (long long)n->float_value : n->int_value;
+    int x = pos / COC_CANVAS_SIZE;
+    int y = pos % COC_CANVAS_SIZE;
+    coc_set_pixel(canvas, x, y, draw_color); 
+}
+
+void act_gfx_color(Number *n) {
+    uint32_t c = n->is_float ? (uint32_t)n->float_value : (uint32_t)n->int_value;
+    uint8_t r = (c >> 16) & 0xFF;
+    uint8_t g = (c >> 8) & 0xFF;
+    uint8_t b = c & 0xFF;
+    draw_color = RGB(r, g, b);
+}
+
+void act_gfx_clear(Number *n) {
+    uint32_t c = n->is_float ? (uint32_t)n->float_value : (uint32_t)n->int_value;
+    uint8_t r = (c >> 16) & 0xFF;
+    uint8_t g = (c >> 8) & 0xFF;
+    uint8_t b = c & 0xFF;
+    coc_canvas_clear(canvas, RGB(r, g, b));
+}
+
+void act_gfx_up(Number *n) {
+    long long pos = n->is_float ? (long long)n->float_value : n->int_value;
+    int x = pos / COC_CANVAS_SIZE;
+    int y = pos % COC_CANVAS_SIZE;
+    y = (y - 1 + COC_CANVAS_SIZE) % COC_CANVAS_SIZE;
+    n->int_value = x * COC_CANVAS_SIZE + y;
+    coc_set_pixel(canvas, x, y, draw_color); 
+}
+
+void act_gfx_down(Number *n) {
+    long long pos = n->is_float ? (long long)n->float_value : n->int_value;
+    int x = pos / COC_CANVAS_SIZE;
+    int y = pos % COC_CANVAS_SIZE;
+    y = (y + 1) % COC_CANVAS_SIZE;
+    n->int_value = x * COC_CANVAS_SIZE + y;
+    coc_set_pixel(canvas, x, y, draw_color); 
+}
+
+void act_gfx_left(Number *n) {
+    long long pos = n->is_float ? (long long)n->float_value : n->int_value;
+    int x = pos / COC_CANVAS_SIZE;
+    int y = pos % COC_CANVAS_SIZE;
+    x = (x - 1 + COC_CANVAS_SIZE) % COC_CANVAS_SIZE;
+    n->int_value = x * COC_CANVAS_SIZE + y;
+    coc_set_pixel(canvas, x, y, draw_color); 
+}
+
+void act_gfx_right(Number *n) {
+    long long pos = n->is_float ? (long long)n->float_value : n->int_value;
+    int x = pos / COC_CANVAS_SIZE;
+    int y = pos % COC_CANVAS_SIZE;
+    x = (x + 1) % COC_CANVAS_SIZE;
+    n->int_value = x * COC_CANVAS_SIZE + y;
+    coc_set_pixel(canvas, x, y, draw_color); 
+}
+
+static inline void register_user_actions(Interpreter *intp) {
+    register_act(intp, "plot",  act_gfx_plot);
+    register_act(intp, "color", act_gfx_color);
+    register_act(intp, "clear", act_gfx_clear);
+    register_act(intp, "up",    act_gfx_up);
+    register_act(intp, "down",  act_gfx_down);
+    register_act(intp, "left",  act_gfx_left);
+    register_act(intp, "right", act_gfx_right);
+}
+
 int run_file(const char *filename, void (*register_user_actions)(Interpreter *)) {
     Coc_String source = {0};
     if (coc_read_entire_file(filename, &source) != 0) return errno;
@@ -495,6 +569,8 @@ int main(int argc, char *argv[]) {
         coc_log(COC_ERROR, "Usage: %s <file name>", argv[0]);
         return 1;
     }
-    run_file(argv[1], NULL);
+    FILE *fp = coc_canvas_init(canvas, "output.ppm");
+    run_file(argv[1], register_user_actions);
+    coc_canvas_save(canvas, fp);
     return 0;
 }
