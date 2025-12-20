b57657e1ccb89d38be14c98fb32c734733d8a234
CoccusQ <150150207+CoccusQ@users.noreply.github.com>
2025-12-20 13:54:13 +0800

    匹配最新的日志打印功能

--- puncta.c
+++ puncta.c
@@ -1,11 +1,15 @@
 #include "puncta.h"
 
+Coc_Log_Config coc_log_config;
+
 int main(int argc, char *argv[]) {
+    coc_log_init(COC_INFO, true, false, NULL);
     if (argc < 2) {
         coc_log(COC_ERROR, "Usage: %s <file name>", argv[0]);
         return 1;
     }
     VM *vm = run_file(argv[1], NULL);
     vm_free(vm);
+    coc_log_close();
     return 0;
 }
